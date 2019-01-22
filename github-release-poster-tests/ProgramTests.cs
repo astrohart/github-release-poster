c7fd1bba978c4ee93130f9563132cb2d8991e320
astrohart <brian.hart@xylogix.net>
2019-01-22 13:52:47 -0500

    22 Jan 2019 Removed all unit test stubs

--- github-release-poster-tests/ProgramTests.cs
+++ github-release-poster-tests/ProgramTests.cs
@@ -5,11 +5,5 @@ namespace github_release_poster_tests
     [TestFixture]
     public class ProgramTests
     {
-        [Test]
-        public void ProcessCommandLineTest()
-        {
-            //TestCop
-            Assert.Fail("WriteMe");
-        }
     }
 }
