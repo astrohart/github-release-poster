8d624696f25cb18bdc03fb2bd86d65a610851191
astrohart <brian.hart@xylogix.net>
2019-01-22 10:53:50 -0500

    22 Jan 2019 Added unit test with full set of args

--- github-release-poster-tests/CommandLineInfoTests.cs
+++ github-release-poster-tests/CommandLineInfoTests.cs
@@ -1,13 +1,39 @@
-﻿using System;
-using System.Linq;
-using github_release_poster;
+﻿using github_release_poster;
 using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace github_release_poster_tests
 {
     [TestFixture]
     public class CommandLineInfoTests
     {
+        /// <summary>
+        /// Valid command-line arguments for testing.  This set of arguments represents the full set, including optional ones.
+        /// </summary>
+        public static string[] ValidFullTestingArgs { get; } =
+        {
+            "--repo-owner",
+            "astrohart",
+            "--repo-name",
+            " github-release-poster",
+            "--user-access-token",
+            $"{Guid.NewGuid().ToString().Replace("-", string.Empty)}",
+            "--target-branch",
+            "master",
+            $"--tag-name",
+            $"{Guid.NewGuid()}",
+            "--release-asset-dir",
+            @"C:\Users\ENS Brian Hart\source\repos\github-release-poster\github-release-poster\bin\x64\Debug",
+            "--name",
+            $"{Guid.NewGuid()}",
+            "--is-draft",
+            "--is-pre-release",
+            "--body",
+            "Testing foo bar",
+            "--no-zip"
+        };
+
         /// <summary>
         /// Valid command-line arguments for testing.  Since we have sensitive information (i.e., my user access token) in this file,
         /// it will not be pushed to the repo in the cloud.
@@ -31,18 +57,6 @@ namespace github_release_poster_tests
             $"{Guid.NewGuid()}"
         };
 
-        /// <summary>
-        /// Tests parsing the command line with a valid set of arguments.
-        /// </summary>
-        [Test]
-        public void ParseCommandLineWithValidArgs()
-        {
-            Assert.IsTrue(
-                CommandLineInfo.Instance.ParseCommandLine(ValidTestingArgs));
-
-            Assert.IsTrue(CommandLineInfo.Instance.WasCommandLineParsed);
-        }
-
         [Test]
         public void ParseCommandLineTesWithNoArgs()
         {
@@ -51,6 +65,31 @@ namespace github_release_poster_tests
             );
 
             Assert.IsFalse(CommandLineInfo.Instance.WasCommandLineParsed);
+        }
+
+        /// <summary>
+        /// Tests parsing the command line with a valid set of arguments.
+        /// </summary>
+        [Test]
+        public void ParseCommandLineWithValidArgs()
+        {
+            Assert.IsTrue(ValidTestingArgs.Any());
+
+            Assert.IsTrue(
+                CommandLineInfo.Instance.ParseCommandLine(ValidTestingArgs));
+
+            Assert.IsTrue(CommandLineInfo.Instance.WasCommandLineParsed);
+        }
+
+        [Test]
+        public void ParseCommandLineWithValidFullSetOfArgs()
+        {
+            Assert.IsTrue(ValidFullTestingArgs.Any());
+
+            Assert.IsTrue(
+                CommandLineInfo.Instance.ParseCommandLine(ValidTestingArgs));
+
+            Assert.IsTrue(CommandLineInfo.Instance.WasCommandLineParsed);
         }
     }
 }
