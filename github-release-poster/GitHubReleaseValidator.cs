16491954ed1733445c1e8719f6083094e0227a8a
astrohart <brian.hart@xylogix.net>
2019-01-22 14:01:05 -0500

    22 Jan 2019 Added implementation code to GitHubReleaseValidator class

--- github-release-poster/GitHubReleaseValidator.cs
+++ github-release-poster/GitHubReleaseValidator.cs
@@ -8,9 +8,16 @@ namespace github_release_poster
         {
             Console.WriteLine("Validating release metadata...");
 
-            // TODO: Add more checks here
+            var result = release != null
+                     && !string.IsNullOrWhiteSpace(release.tag_name)
+                     && !string.IsNullOrWhiteSpace(release.name)
+                     && !string.IsNullOrWhiteSpace(release.target_commitish);
 
-            return release != null;
+            Console.WriteLine(result
+                ? "Release data has been validated.  No errors have been found."
+                : "Failed to validate the release data.");
+
+            return result;
         }
     }
 }
