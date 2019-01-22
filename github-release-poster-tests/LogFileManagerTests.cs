1a01876f83795d05dcb2f9bc61be68a248a9d9aa
astrohart <brian.hart@xylogix.net>
2019-01-22 09:13:48 -0500

    22 Jan 2019 Put back a unit test that got deleted

--- github-release-poster-tests/LogFileManagerTests.cs
+++ github-release-poster-tests/LogFileManagerTests.cs
@@ -1,10 +1,30 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using github_release_poster;
+using NUnit.Framework;
 
 namespace github_release_poster_tests
 {
     [TestFixture]
     public class LogFileManagerTests
     {
+        /// <summary>
+        /// Path to the application configuration file.
+        /// </summary>
+        private const string AppConfigPath = 
+            @"C:\Users\ENS Brian Hart\source\repos\github-release-poster\github-release-poster\App.config";
 
+        /// <summary>
+        /// Tests the <see cref="M:github_release_poster.LogFileManager.InitializeLogging"/> method.
+        /// </summary>
+        [Test]
+        public void InitializeLoggingTest()
+        {
+            Assert.IsTrue(File.Exists(AppConfigPath));
+
+            LogFileManager.InitializeLogging(false,
+                true, AppConfigPath);
+
+            Assert.IsTrue(LogFileManager.IsLoggingInitialized, "Failed to initialize the log file.");
+        }
     }
 }
