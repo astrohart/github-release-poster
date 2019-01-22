8d5c123541a55fc6d1e329db87463424acf4ee57
astrohart <brian.hart@xylogix.net>
2019-01-22 13:42:38 -0500

    22 Jan 2019 Added unit test for the NewReleaseFactory.cs class

---
+++ github-release-poster-tests/ProgramTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace github_release_poster_tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void ProcessCommandLineTest()
+        {
+            //TestCop
+            Assert.Fail("WriteMe");
+        }
+    }
+}
