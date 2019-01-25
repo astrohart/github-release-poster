48d5e3bcf697a610b5a81407df379a40d2ceda90
astrohart <brian.hart@xylogix.net>
2019-01-25 09:01:01 -0500

    25 Jan 2019 Removed unused references

--- github-release-poster-tests/Class1.cs
+++ github-release-poster-tests/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace github_release_poster_tests
+﻿namespace github_release_poster_tests
 {
     public class Class1
     {
