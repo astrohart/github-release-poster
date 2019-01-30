4fb2f47c8ba09081bd5d7612d1bb0bad8e05bef7
astrohart <brian.hart@xylogix.net>
2019-01-30 11:26:35 -0500

    30 Jan 2019 Added a IsLocked method that tells whether a FileSystemInfo entry is in use by another process

--- github-release-poster/Extensions/FileSystemInfoExtensions.cs
+++ github-release-poster/Extensions/FileSystemInfoExtensions.cs
@@ -3,7 +3,7 @@
 namespace GitHubReleasePoster.Extensions
 {
     /// <summary>
-    /// Provides extension methods for instances of <see cref="T:System.IO.FileSystemInfo"/>. 
+    /// Provides extension methods for instances of <see cref="T:System.IO.FileSystemInfo"/>.
     /// </summary>
     public static class FileSystemInfoExtensions
     {
@@ -23,6 +23,53 @@ namespace GitHubReleasePoster.Extensions
                 return false;
 
             return (info.Attributes & FileAttributes.Directory) == FileAttributes.Directory;
+        }
+
+        /// <summary>
+        /// Checks whether the specified <see cref="file"/> is locked for reading/writing
+        /// by another process or thread.
+        /// </summary>
+        /// <param name="file">Reference to an instance of an object that is derived from
+        /// <see cref="T:System.IO.FileSystemInfo"/> that refers to a file system entry. The
+        /// entry must be that for a file.
+        /// </param>
+        /// <returns>True if the file is locked by someone else; false otherwise.</returns>
+        /// <remarks>For obvious reasons, a unit test of this method does not exist, since
+        /// files become locked and unlocked by processes at random, so we can't hope to test
+        /// this deterministically.</remarks>
+        public static bool IsLocked(this FileSystemInfo file)
+        {
+            // Can't check a null FileInfo -- tell callers it's locked
+            // so they won't try to access it
+            if (file == null)
+                return true;
+
+            if (!file.Exists)
+                return true;    // Don't try to open a non-existent file!
+
+            if (file.IsFolder())
+                return true;    // Don't try to check a folder
+
+            // Attempt to open a file stream for reading on the file. If
+            // an exception gets thrown, then we know we can't touch it right now
+            var stream = default(FileStream);
+
+            try
+            {
+                stream = File.OpenRead(file.FullName);
+            }
+            catch
+            {
+                //if we are here, then the file is unavailable
+                return true;
+            }
+            finally
+            {
+                stream?.Close();
+            }
+
+            // if we are here, then the file is available
+            return false;
         }
     }
 }
