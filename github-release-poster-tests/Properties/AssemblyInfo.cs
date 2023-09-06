c91732019d28c86854c39d4db64363c13f2a1612
Brian Hart <brian.hart@xylogix.net>
2023-09-06 00:55:47 -0600

    Latest updates at 00:55 hours MDT on 09/06/2023

--- github-release-poster-tests/Properties/AssemblyInfo.cs
+++ github-release-poster-tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("github-release-poster-tests")]
-[assembly: AssemblyCopyright("Copyright ?  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
